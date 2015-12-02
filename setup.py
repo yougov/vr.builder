251d04f5ba9b66f0ef0c0580c626c34eb887cb48
Darwin Monroy <contact@darwinmonroy.com>
2015-12-02 09:32:06 -0600

    bump version from 1.2.3 to 1.3.0

--- setup.py
+++ setup.py
@@ -5,18 +5,18 @@ setup(
     name='vr.builder',
     description=('Command line tools to build apps in containers.'),
     namespace_packages=['vr'],
-    version='1.2.3',
+    version='1.3.0',
     author='Brent Tubbs',
     author_email='brent.tubbs@gmail.com',
     url='https://bitbucket.org/yougov/vr.builder',
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
-        'vr.runners>=2.4.4,<3',
+        'vr.runners>=2.8',
         'path.py>=7.1',
         'yg.lockfile',
         'jaraco.itertools',
-        'vr.common>=4.2.0',
+        'vr.common>=4.3.0',
     ],
     entry_points={
         'console_scripts': [
